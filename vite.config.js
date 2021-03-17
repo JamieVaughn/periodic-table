84b931232b42dc3b4c583b0191b4d9cdae96d757
JamieVaughn <wxv6@case.edu>
2021-03-17 10:44:07 -0400

    Build with base path corrected

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  base: './',
+  base: '/periodic-table/',
   build: {
     outDir: 'docs'
   }
